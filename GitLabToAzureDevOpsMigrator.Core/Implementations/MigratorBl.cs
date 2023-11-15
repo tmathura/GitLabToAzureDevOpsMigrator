a5eaf779eb887e8b67c3c41eb69e8b54e36b9024
tmathura <tmathura@gmail.com>
2023-11-15 23:10:46 +0100

    collect issues in parallel

--- GitLabToAzureDevOpsMigrator.Core/Implementations/MigratorBl.cs
+++ GitLabToAzureDevOpsMigrator.Core/Implementations/MigratorBl.cs
@@ -1,4 +1,5 @@
 ﻿using GitLabToAzureDevOpsMigrator.Core.Interfaces;
+using GitLabToAzureDevOpsMigrator.Domain.Models;
 using GitLabToAzureDevOpsMigrator.Domain.Models.GitLab;
 using GitLabToAzureDevOpsMigrator.GitLabWrapper.Interfaces;
 using log4net;
@@ -31,7 +32,7 @@ namespace GitLabToAzureDevOpsMigrator.Core.Implementations
                 Console.WriteLine($"This is epic {epic.EpicIid} - '{epic.Title}'");
             }
         }
-
+        
         public async Task CollectGitLabIssues(string gitLabUrl, int gitLabGroupId, int gitLabProjectId, string gitLabAccessToken)
         {
             var statisticsRoot = await ProjectService.GetIssuesStatistics(gitLabProjectId, new List<string> { "team::Core" });
@@ -48,7 +49,7 @@ namespace GitLabToAzureDevOpsMigrator.Core.Implementations
 
             var allIssuesCount = statisticsRoot.Statistics.Counts.All;
 
-            var startingProcessMessage = $"Collecting GitLab issues, there are {allIssuesCount} issues to collect.";
+            var startingProcessMessage = $"Started collecting GitLab issues, there are {allIssuesCount} issues to collect.";
 
             Console.WriteLine(startingProcessMessage);
             Logger.Info(startingProcessMessage);
@@ -64,53 +65,77 @@ namespace GitLabToAzureDevOpsMigrator.Core.Implementations
             var issues = client.Issues.GetAsync(gitLabProjectId, new IssueQuery { Labels = "team::Core" });
 
             var count = 0;
-            var errorCount = 0;
-
             var gitLabIssues = new List<FullIssueDetails>();
+
+            var semaphore = new SemaphoreSlim(10); // Set the maximum number of parallel tasks
+            var tasks = new List<Task<ProcessIssueResult>>();
 
             await foreach (var issue in issues)
             {
                 count++;
 
-                try
-                {
-                    var gitLabIssue = new FullIssueDetails(issue, new List<Attachment>(), new List<ProjectIssueNote>(), new List<Attachment>(), new List<Issue>());
+                await semaphore.WaitAsync(); // Wait until the semaphore is available
 
-                    GetAttachmentInString(issue.Description, projectUrl, projectUrlSegments, gitLabIssue.IssueAttachments);
-
-                    var notes = projectIssueNoteClient.ForIssue(issue.IssueId);
-
-                    foreach (var note in notes)
-                    {
-                        gitLabIssue.Notes.Add(note);
-
-                        GetAttachmentInString(note.Body, projectUrl, projectUrlSegments, gitLabIssue.NotesAttachments);
-                    }
-
-                    var relatedIssues = client.Issues.LinkedToAsync(gitLabProjectId, issue.IssueId);
-
-                    await foreach (var relatedIssue in relatedIssues)
-                    {
-                        gitLabIssue.RelatedIssues.Add(relatedIssue);
-                    }
-
-                    gitLabIssues.Add(gitLabIssue);
-
-                    Logger.Info($"Collected {count} GitLab issues so far, issue #{issue.IssueId} - '{issue.Title}' was just collected. ");
-
-                    DrawConsoleProgressBar(count, allIssuesCount);
-                }
-                catch (Exception exception)
-                {
-                    errorCount++;
-                    Logger.Error($"Error collecting GitLab issue #{issue.IssueId} - '{issue.Title}', was on issue count: {count}.", exception);
-                }
+                tasks.Add(ProcessIssueAsync(issue, gitLabProjectId, projectIssueNoteClient, client, projectUrl, projectUrlSegments, gitLabIssues, count, allIssuesCount, semaphore));
             }
 
-            var endingProcessMessage = $"{Environment.NewLine}Collecting GitLab issues complete, {count} issues collected. Error collect {errorCount} issues.";
+            var processedResults = await Task.WhenAll(tasks);
+
+            var processedCount = processedResults.Sum(result => result.Count);
+            var errorCount = processedResults.Sum(result => result.ErrorCount);
+
+            var endingProcessMessage = $"{Environment.NewLine}Finished collecting GitLab issues, there are {processedCount} issues collected & there were errors collecting {errorCount} issues.";
 
             Console.WriteLine(endingProcessMessage);
             Logger.Info(endingProcessMessage);
+        }
+
+        private async Task<ProcessIssueResult> ProcessIssueAsync(Issue issue, int gitLabProjectId, IProjectIssueNoteClient projectIssueNoteClient, IGitLabClient client, string projectUrl, string projectUrlSegments, ICollection<FullIssueDetails> gitLabIssues, int count, int allIssuesCount, SemaphoreSlim semaphore)
+        {
+            var processIssueResult = new ProcessIssueResult();
+
+            try
+            {
+                var gitLabIssue = new FullIssueDetails(issue, new List<Attachment>(), new List<ProjectIssueNote>(), new List<Attachment>(), new List<Issue>());
+
+                GetAttachmentInString(issue.Description, projectUrl, projectUrlSegments, gitLabIssue.IssueAttachments);
+
+                var notes = projectIssueNoteClient.ForIssue(issue.IssueId);
+
+                foreach (var note in notes)
+                {
+                    gitLabIssue.Notes.Add(note);
+
+                    GetAttachmentInString(note.Body, projectUrl, projectUrlSegments, gitLabIssue.NotesAttachments);
+                }
+
+                var relatedIssues = client.Issues.LinkedToAsync(gitLabProjectId, issue.IssueId);
+
+                await foreach (var relatedIssue in relatedIssues)
+                {
+                    gitLabIssue.RelatedIssues.Add(relatedIssue);
+                }
+                
+                gitLabIssues.Add(gitLabIssue);
+
+                DrawConsoleProgressBar(count, allIssuesCount);
+
+                Logger.Info($"Collected {count} GitLab issues so far, issue #{issue.IssueId} - '{issue.Title}' was just collected. ");
+
+                processIssueResult.Count = 1;
+            }
+            catch (Exception exception)
+            {
+                Logger.Error($"Error collecting GitLab issue #{issue.IssueId} - '{issue.Title}', was on issue count: {count}.", exception);
+
+                processIssueResult.ErrorCount = 1;
+            }
+            finally
+            {
+                semaphore.Release(); // Release the semaphore when the processing is complete
+            }
+
+            return processIssueResult;
         }
 
         private static void GetAttachmentInString(string stringToExtractAttachment, string projectUrl, string projectUrlSegments, ICollection<Attachment> attachments)
---
+++ GitLabToAzureDevOpsMigrator.Domain/Models/ProcessIssueResult.cs
@@ -0,0 +1,8 @@
+﻿namespace GitLabToAzureDevOpsMigrator.Domain.Models
+{
+    public class ProcessIssueResult
+    {
+        public int Count { get; set; }
+        public int ErrorCount { get; set; }
+    }
+}
