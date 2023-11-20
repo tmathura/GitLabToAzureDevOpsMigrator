e1714591ac52ca1b90bea4b0d910174700bbdd15
tmathura <tmathura@gmail.com>
2023-11-20 23:44:54 +0100

    lock when drawing progress

--- GitLabToAzureDevOpsMigrator.Core/Implementations/ConsoleHelper.cs
+++ GitLabToAzureDevOpsMigrator.Core/Implementations/ConsoleHelper.cs
@@ -5,33 +5,54 @@ namespace GitLabToAzureDevOpsMigrator.Core.Implementations
     public class ConsoleHelper : IConsoleHelper
     {
         private int Progress { get; set; }
-        
+        private readonly object _lock = new();
+
         public void DrawConsoleProgressCount(int count)
         {
-            Console.CursorLeft = 0;
-            
-            Console.Write("[");
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.Write($" Progress: {count}");
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write("]");
+            if (Monitor.TryEnter(_lock))
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.CursorLeft = 0;
+                    Console.Write("[");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Write($" Progress: {count}");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write("]");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
         }
 
         public void DrawConsoleProgressBar(int total)
         {
-            Progress++;
-
-            Console.CursorLeft = 0;
-
-            const int progressBarLength = 100;
-            var filledLength = (int)Math.Floor((double)Progress / total * progressBarLength);
-
-            Console.Write("[");
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.Write(new string(' ', filledLength));
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write(new string(' ', progressBarLength - filledLength));
-            Console.Write($"] {Progress * 100 / total}%");
+            if (Monitor.TryEnter(_lock))
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Progress++;
+                    Console.CursorLeft = 0;
+                    const int progressBarLength = 100;
+                    var filledLength = (int)Math.Floor((double)Progress / total * progressBarLength);
+                    Console.Write("[");
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Write(new string(' ', filledLength));
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(new string(' ', progressBarLength - filledLength));
+                    Console.Write($"] {Progress * 100 / total}%");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
         }
     }
 }
